f4ca93e19d5cee1fd3152b7de525a55f7a12e4f8
Robin Roelandt <Robin.Roelandt@iMacC.local>
2025-10-09 12:37:59 +0200

    first commit

---
+++ RpnCalculator/RpnCalculator/RpnCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RpnCalculatorApp.swift
+//  RpnCalculator
+//
+//  Created by Robin Roelandt on 09/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct RpnCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
