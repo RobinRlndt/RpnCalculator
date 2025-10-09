f695b19097a993118508a2637379c1827f1e8488
Robin Roelandt <Robin.Roelandt@iMacC.local>
2025-10-09 12:58:27 +0200

    commit 2

---
+++ RpnCalculator/RpnCalculator/RpnCalcView.swift
@@ -0,0 +1,25 @@
+//
+//  RpnCalcView.swift
+//  RpnCalculator
+//
+//  Created by Robin Roelandt on 09/10/2025.
+//
+
+import SwiftUI
+
+struct RpnCalcView: View {
+    var body: some View {
+        HStack {
+            VStack {
+                
+            }
+            Grid {
+                
+            }
+        }.BackgroundStyle(.gray)
+    }
+}
+
+#Preview {
+    RpnCalcView()
+}
---
+++ RpnCalculator/RpnCalculator/calcEngine.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  RpnCalculator
+//
+//  Created by Robin Roelandt on 09/10/2025.
+//
+
+import Foundation
+
+@Observable
+class calcEngine {
+    var x: [Double] = []
+    
+    func increment() {
+        
+    }
+    
+    func decrement() {
+        
+    }
+    
+    func multiply() {
+        
+    }
+    
+    func divide() {
+        
+    }
+}
